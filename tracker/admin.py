2ce26123e240d21c9e07782bcf8a6ffea2f5694a
Shashank Mishra <mishrashashank2106@gmail.com>
2025-07-25 16:26:44 +0530

    Register models in admin panel and apply initial migrations

---
+++ tracker/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Bug, Comment
+
+@admin.register(Bug)
+class BugAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_by', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('title', 'description', 'created_by__username')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('bug', 'author', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('text', 'author__username')
