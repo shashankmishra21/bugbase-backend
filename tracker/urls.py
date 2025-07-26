9f7272884807259e61468555ee855349b252acff
Shashank Mishra <mishrashashank2106@gmail.com>
2025-07-26 22:01:38 +0530

    deployment update 3  backend

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,12 +5,13 @@ from .auth_views import register_user
 from .views import get_user_info
 from .views import CommentListCreateView
 from .views import export_bugs_csv
+from .views import LoginView
 
 urlpatterns = [
     path('bugs/', BugListCreateView.as_view(), name='bug-list-create'),
     path('bugs/<int:pk>/', BugDetailView.as_view(), name='bug-detail'),
     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
-    path('login/', obtain_auth_token, name='api_token_auth'),
+   path('login/', LoginView.as_view(), name='login'),
     path('register/', register_user, name='register'),
     path('user/', get_user_info, name='get-user-info'),
     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
